3a86f640b246675807d4762c3f2e319944496b48
EmmanuelLopez-Vanegas <146570978+EmmanuelLopez-Vanegas@users.noreply.github.com>
2025-04-05 10:37:16 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import time 
-import tkinter
-import 
-
-
-=======
 #Modules
 import random
 import tkinter
 
 #List of outdoor items
 items = ["Stop sign", "Flower", "Clouds", "Person walking their pet (with consent)", "Hiking trail sign", "A very old tree"]
->>>>>>> 39e1e381463f076994e6b3f83e2540366574a5d2
