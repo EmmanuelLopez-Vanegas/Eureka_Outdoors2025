7e3f1de4547d8481e9e550888807b5d49d5885b9
realthrongler <fjack6677@gmail.com>
2025-04-05 13:35:57 -0400

    more tkinter content

--- code/main.py
+++ code/main.py
@@ -2,11 +2,17 @@
 import random
 import tkinter
 import datetime 
+from tkinter import filedialog
 
 #List of outdoor items
 items = ["stop sign", "flower", "clouds"]
 
+#drawing window with tkinter, some basic setup
 root = tkinter.Tk()
 root.title("testing testing one two three *taps mic* is this thing on")
 
+#UI design
+file_upload_button = tkinter.Button(root, text="+", padx=5, pady=5)
+file_upload_button.grid(row=1, column=1, padx=18)
+#looping window to allow for user interaction
 root.mainloop()
