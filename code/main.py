a8ef5b2dadbb6cb298c12882897ec7116e6c81a8
realthrongler <fjack6677@gmail.com>
2025-04-05 13:06:46 -0400

    tkinter testing

--- code/main.py
+++ code/main.py
@@ -6,3 +6,7 @@ import datetime
 #List of outdoor items
 items = ["stop sign", "flower", "clouds"]
 
+root = tkinter.Tk()
+root.title("testing testing one two three *taps mic* is this thing on")
+
+root.mainloop()
