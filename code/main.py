bde3dc024ba8667bfdc2f3bbee0cf15f31743cbd
realthrongler <fjack6677@gmail.com>
2025-04-05 10:37:14 -0400

    imported time

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 #Modules
 import random
 import tkinter
+import time
 
 #List of outdoor items
-items = ["Stop sign", "Flower", "Clouds", "Person walking their pet (with consent)", "Hiking trail sign", "A very old tree"]
+items = ["Stop sign", "Flower", "Clouds", "Person walking their pet (with consent)", "Hiking trail sign", "A very old tree", "Pile of leaves", "Snow pile"]
