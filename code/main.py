64708198908627f6042ad09fd4114e0f7aa80a8a
realthrongler <fjack6677@gmail.com>
2025-04-05 10:48:39 -0400

    more groundwork

--- code/main.py
+++ code/main.py
@@ -1,7 +1,11 @@
 #Modules
 import random
 import tkinter
-import time
+import datetime
 
 #List of outdoor items
-items = ["Stop sign", "Flower", "Clouds", "Person walking their pet (with consent)", "Hiking trail sign", "A very old tree"]
+items_generic = ["Stop sign", "Flower", "Clouds", "Person walking their pet (with consent)", "Hiking trail sign", "A very old tree", "Pickup truck", ""]
+items_winter = [""]
+items_summer = [""]
+items_spring = [""]
+
